97b6fa1d1f4a90f37963571b257ec4655ea3eaf3
Shwetakshi Priya <shwetakshi1175.be21@chitkarauniversity.edu.in>
2024-03-26 15:02:36 +0530

    sdfgh

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT || 3000; // Use the PORT environment variable provided by Render.com, or default to 3000
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => { // Listen on all network interfaces
   console.log(`Server is running on port ${port}`);
 });
